d71c6d2b513cc0ba9387858de80a98677eb12f36
Md. Mahbub hiron <mhalom19@gmail.com>
2025-01-27 03:07:24 +0600

    get data from database

--- index.js
+++ index.js
@@ -29,12 +29,17 @@ async function run() {
 
     const menuCollection = client.db("restaurantDb").collection("menu");
 
+    app.get('/menu', async(req, res)=>{
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
