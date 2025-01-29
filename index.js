a5b7174978d537029a147ff186f1e026e5663ef3
Md. Mahbub hiron <mhalom19@gmail.com>
2025-01-29 18:11:32 +0600

    implement to make an admin role

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ async function run() {
     const cartCollection = client.db("restaurantDb").collection("carts");
 
     // user related api
+    app.get('/users', async(req, res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req, res)=>{
       const user = req.body;
       // insert email if user does not exists 
@@ -42,6 +47,25 @@ async function run() {
         return res.send({message: 'user already exists', insertedId: null})
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set:{
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
